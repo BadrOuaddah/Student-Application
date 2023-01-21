13b9b9634a71b3eec88ad59e4b7b8562e8619b85
Admin <Badr.Ouaddah.07@gmail.com>
2023-01-21 20:22:13 +0100

    Create two components

---
+++ front-end/student-react/src/components/navigation-bar/navigationBar.js
@@ -0,0 +1,12 @@
+import "./navigationBar.css"
+import React from 'react'
+
+const navigationBar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default navigationBar
---
+++ front-end/student-react/src/components/student/student.js
@@ -0,0 +1,12 @@
+import "./student.css"
+import React from 'react'
+
+const student = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default student
