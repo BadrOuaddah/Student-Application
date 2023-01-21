da0d57b3d5048d1fe6a5af4d0949427d343b35ec
Admin <Badr.Ouaddah.07@gmail.com>
2023-01-21 22:08:20 +0100

    Testing first REACT App with some modify

---
+++ front-end/student-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1>
+        Welcome to my first REACT App (JavaScript) &#128187;
+      </h1>
+    </div>
+  )
+}
+
+export default App
