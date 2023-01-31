3de921957a5a641a4757b836a8d5b9268cfe20f2
Admin <Badr.Ouaddah.07@gmail.com>
2023-01-31 18:38:47 +0100

    Try and experiment in the React application

--- front-end/student-react/src/App.js
+++ front-end/student-react/src/App.js
@@ -1,60 +1,32 @@
 import React from "react";
 import "./App.css";
-
+import "bootstrap";
 const App = () => {
   return (
     <div>
       <h1>Welcome to my first REACT App (JavaScript) &#128187;</h1>
       <p>
-        <h2>
-          loremDolore deserunt laboris pariatur nulla laboris amet officia esse
-          enim duis aliquip pariatur. In magna do culpa nostrud qui ea occaecat
-          est nisi ea. Mollit non duis id dolore ipsum officia ex mollit
-          voluptate ipsum dolor. Nisi incididunt ex Lorem culpa sit deserunt
-          occaecat consequat adipisicing.
-        </h2>
-        Dolore eiusmod ex voluptate quis tempor velit mollit. Fugiat excepteur
-        ad magna occaecat duis ut ea irure pariatur esse. Consectetur ipsum nisi
-        excepteur culpa nisi. Eu adipisicing qui eiusmod non officia incididunt
-        est elit id minim Lorem fugiat Lorem. Proident cillum exercitation qui
-        pariatur cillum anim tempor elit amet velit mollit elit ipsum
-        exercitation. Culpa amet mollit cupidatat nostrud velit adipisicing.
-        Duis esse eiusmod excepteur eu nisi esse excepteur quis laborum
-        voluptate exercitation minim. Qui sit est fugiat dolore amet do nisi
-        occaecat ex sunt occaecat culpa cupidatat aliquip. Sunt excepteur do
-        sint ipsum enim velit cupidatat esse labore. Sint est id excepteur id
-        quis veniam amet sint est. Elit in dolore commodo reprehenderit mollit
-        aute laborum veniam laborum. Aliqua minim aute ad velit dolore proident
-        et aute adipisicing magna amet duis. Magna velit dolor ad enim velit
-        aliqua veniam minim. Ea eu pariatur duis esse. Ea reprehenderit aliquip
-        labore consequat anim incididunt cupidatat veniam eiusmod ipsum mollit
-        aute ad. Do tempor et duis deserunt commodo sit deserunt minim
-        exercitation dolor ea incididunt. Id elit duis velit eu est qui ut
-        aliqua mollit nisi dolor et culpa. Quis Lorem consectetur eiusmod elit
-        nostrud esse deserunt cillum. Minim deserunt Lorem deserunt consectetur
-        consectetur velit esse eiusmod. Incididunt aliqua sunt cupidatat do enim
-        elit mollit ex aute eu in ut. Dolore occaecat reprehenderit exercitation
-        id incididunt. Dolor ex occaecat do reprehenderit irure. Occaecat quis
-        minim velit id est tempor esse laborum amet. Est sunt dolor esse
-        excepteur minim sint ex nostrud ad laboris. Ad consequat eiusmod minim
-        culpa ut deserunt labore id. Est ex culpa cupidatat officia irure do
-        dolore ex aliqua. Qui officia cupidatat eiusmod aute. Veniam magna et
-        mollit amet deserunt officia ullamco. Sint enim laboris laborum enim
-        occaecat mollit non incididunt. Tempor dolore in non minim sint
-        cupidatat labore deserunt irure ea ipsum dolor velit cupidatat. Labore
-        sint occaecat nulla non dolore. Culpa culpa ex ea est occaecat sunt
-        aliquip consectetur. Amet reprehenderit pariatur dolor consequat. Duis
-        voluptate pariatur et aliqua laboris ad duis fugiat enim pariatur sit
-        occaecat. Lorem voluptate ad est ut commodo aliqua mollit quis.
-        Consequat id quis qui incididunt minim elit aute. Ex ipsum proident
-        fugiat cupidatat ex anim exercitation excepteur reprehenderit do est.
-        Incididunt esse elit cillum velit dolore officia deserunt tempor elit
-        adipisicing fugiat deserunt ut. Veniam nostrud aute magna laborum elit
-        labore elit dolor adipisicing. Consectetur occaecat laboris anim ipsum
-        cupidatat qui. Laborum ex officia eiusmod in cillum minim amet dolor.
-        Adipisicing eiusmod excepteur do enim esse. Occaecat in sint ipsum
-        cillum. Sit ad laborum sunt consequat minim nostrud culpa.
+        <h4>
+        loremDolore deserunt laboris pariatur nulla laboris amet officia esse
+        enim duis aliquip pariatur. In magna do culpa nostrud qui ea occaecat
+        est nisi ea. Mollit non duis id dolore ipsum officia ex mollit voluptate
+        ipsum dolor. Nisi incididunt ex Lorem culpa sit deserunt occaecat
+        consequat adipisicing.
+        Ea proident pariatur duis cupidatat reprehenderit eu ullamco eiusmod ad excepteur et veniam amet nulla.
+        </h4>
       </p>
+      <ul>
+        <li>items</li>
+        <li>items</li>
+        <li>items</li>
+      </ul>
+      <div>
+        <a href="https://www.google.com/">click here for more...</a>
+      </div>
+      <br />
+        <ul>enter number :
+        <input type="text" />
+        </ul>
     </div>
   );
 };
