81c04c340fa1e3591a5c42b139cbcb0bbe08552c
Admin <Badr.Ouaddah.07@gmail.com>
2023-02-11 14:59:51 +0100

    Add content of edit form in component

--- front-end/student-react/src/components/ShowEditForm/ShowEditForm.jsx
+++ front-end/student-react/src/components/ShowEditForm/ShowEditForm.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import './ShowEditForm.css'
+import React from "react";
+import "./ShowEditForm.css";
 
 export default function ShowEditForm() {
   return (
     <div>
-        Show Edit Form !!!
+      <form>
+        <div className="form-row">
+          <div className="col">
+            <i class="fa fa-user-circle" aria-hidden="true"></i> Name :
+            <input type="text" className="form-control" placeholder="Name" />
+          </div>
+          <div className="col">
+            <i class="fa fa-envelope" aria-hidden="true"></i> Email :
+            <input type="email" className="form-control" placeholder="Email" />
+          </div>
+        </div>
+        <br />
+        <div className="form-row">
+          <div className="col">
+            <i class="fa fa-birthday-cake" aria-hidden="true"></i> Date of
+            birthday :
+            <input type="date" className="form-control" />
+          </div>
+          <div className="col">
+            <i class="fa fa-user-plus" aria-hidden="true"></i> Age :
+            <input type="number" className="form-control" placeholder="Age" />
+          </div>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
