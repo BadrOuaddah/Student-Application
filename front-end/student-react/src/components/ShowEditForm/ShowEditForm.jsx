cc4212ef0d42260a7fc1836c54b71f7fd571b5a7
Admin <Badr.Ouaddah.07@gmail.com>
2023-02-14 01:12:06 +0100

    Update POST method

--- front-end/student-react/src/components/ShowEditForm/ShowEditForm.jsx
+++ front-end/student-react/src/components/ShowEditForm/ShowEditForm.jsx
@@ -14,7 +14,35 @@ export default class ShowEditForm extends Component {
   };
 
   AddStudent(){
-    axios.post(baseURL).then();
+    const nameInput = document.getElementById("name_id");
+    const emailInput = document.getElementById("email_id");
+    const dateOfBirthdayInput = document.getElementById("dob_id");
+    const ageInput = document.getElementById("age_id");
+    const submitButton = document.getElementById("submit_id");
+    
+    submitButton.addEventListener("click",() => {
+      const name = nameInput.value;
+      const email = emailInput.value;
+      const dob = dateOfBirthdayInput.value;
+      const age = ageInput.value;
+
+      axios.post(baseURL,{
+        name:name,
+        email:email,
+        dob:dob,
+        age:age
+      }).then(() =>{
+        const students = this.state.students;
+        this.setState({students});
+      })
+    })
+
+    // then((res) => setData(res.data.id));
+    
+    // response => {
+    //   const students = response.json();
+    //   this.setState({ students });
+    // }
   }
 
   render() {
@@ -25,11 +53,11 @@ export default class ShowEditForm extends Component {
   <div className="form-row">
     <div className="col">
       <i className="fa fa-user-circle" aria-hidden="true"></i> Name :
-      <input type="text" className="form-control" placeholder="Name" />
+      <input type="text" className="form-control" placeholder="Name" id='name_id' />
     </div>
     <div className="col">
       <i className="fa fa-envelope" aria-hidden="true"></i> Email :
-      <input type="email" className="form-control" placeholder="Email" />
+      <input type="email" className="form-control" placeholder="Email" id='email_id' />
     </div>
   </div>
   <br />
@@ -37,16 +65,16 @@ export default class ShowEditForm extends Component {
     <div className="col">
       <i className="fa fa-birthday-cake" aria-hidden="true"></i> Date of
       birthday :
-      <input type="date" className="form-control" />
+      <input type="date" className="form-control" id='dob_id' />
     </div>
     <div className="col">
       <i className="fa fa-user-plus" aria-hidden="true"></i> Age :
-      <input type="number" className="form-control" placeholder="Age" />
+      <input type="number" className="form-control" placeholder="Age" id='age_id' />
     </div>
   </div>
   <br />
   <div className="center">
-  <button type="submit" className="btn btn-primary">Submit</button>
+  <button type="submit" onClick={this.AddStudent} className="btn btn-primary" id='submit_id'>Submit</button>
   </div>
   <br />
 </form>
