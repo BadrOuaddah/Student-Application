2e15071a4975ab65a09513c5920203f57e6e8587
Admin <Badr.Ouaddah.07@gmail.com>
2023-02-23 00:27:51 +0100

    Finally add POST request http and it's working

--- front-end/student-react/src/components/ShowEditForm/ShowEditForm.jsx
+++ front-end/student-react/src/components/ShowEditForm/ShowEditForm.jsx
@@ -35,7 +35,7 @@ export default class ShowEditForm extends Component {
       dob:dob,
       age:age
     })
-    this.setState({ [event.target.name]: event.target.value });
+    // this.setState({ [event.target.name]: event.target.value });
   };
 
   submitHandler = (event) => {
