fa5344724d3b37c772345908716a7ae965aad07f
Admin <Badr.Ouaddah.07@gmail.com>
2023-02-22 19:31:05 +0100

    Update POST request http (update version)

--- front-end/student-react/src/components/ShowEditForm/ShowEditForm.jsx
+++ front-end/student-react/src/components/ShowEditForm/ShowEditForm.jsx
@@ -1,148 +1,118 @@
-import React, { useState } from "react";
-import "./ShowEditForm.css";
 import axios from "axios";
+import React, { Component } from "react";
 
 const baseURL = "http://localhost:8080/api/v1/student";
-export default function ShowEditForm() {
+const nameInput = document.getElementById("name_id");
+const emailInput = document.getElementById("email_id");
+const dateOfBirthdayInput = document.getElementById("dob_id");
+const ageInput = document.getElementById("age_id");
+const name = nameInput.value;
+const email = emailInput.value;
+const dob = dateOfBirthdayInput.value;
+const age = ageInput.value;
+const studentArray = {
+  name,
+  email,
+  dob,
+  age,
+};
 
-  const [student, setStudent] = useState({
-    name: "",
-    email: "",
-    dob: "",
-    age: 0,
-  });
+export default class ShowEditForm extends Component {
+  
+  
 
-  // const handleInput = (event) => {
-  //   const nameInput = document.getElementById("name_id");
-  //   const emailInput = document.getElementById("email_id");
-  //   const dateOfBirthdayInput = document.getElementById("dob_id");
-  //   const ageInput = document.getElementById("age_id");
-
-  //   const name = nameInput.value;
-  //   const email = emailInput.value;
-  //   const dob = dateOfBirthdayInput.value;
-  //   const age = ageInput.value;
-    
-  //   const studentArray = {
-  //     name,email,dob,age
-  //   }
-
-  //   const student = JSON.stringify(studentArray)
-
-
-  //   //! bug
-  //   setStudent({...student, [event.target.name]: event.target.event})
- // };
-  const handleSubmit = async (event) => {
-
-    const nameInput = document.getElementById("name_id");
-    const emailInput = document.getElementById("email_id");
-    const dateOfBirthdayInput = document.getElementById("dob_id");
-    const ageInput = document.getElementById("age_id");
-
-    const name = nameInput.value;
-    const email = emailInput.value;
-    const dob = dateOfBirthdayInput.value;
-    const age = ageInput.value;
-    
-    const studentArray = {
-      name,email,dob,age
+  constructor(props){
+    super(props)
+    this.state ={
+      name: "",
+      email: "",
+      dob: "",
+      age: 0,
     }
-
-    // const student = JSON.stringify(studentArray)
-
-
-    //! bug
-    // setStudent({...student, [event.target.name]: event.target.event})
-    
-    axios.post(baseURL,{
-      name: "basim",
-      email: "basim@email",
-      dob: "2000-01-05",
-      age: 1,
-    },{headers: {'Content-Type': 'application/json'}}).then((response) => {
-      console.log(response);
-    }, (error) => {
-      console.log(error);
-    });
-    
-    
-    
-    // axios.post(baseURL,{student
-    // }).then(() =>{
-    //   const students = event;
-    //   this.setState({students});
-    // })
-
-
-    // then(() =>{
-      //   const students = this.state.students;
-    //   this.setState({students});
-
-    // event.preventDefault()
-    // await axios.post(baseURL,{student}).then(response => console.log(response)).catch(err => console.log(err))
   }
 
 
-  return (
-    <div>
-      <div className="boxShadow">
-        <form>
-        {/* onSubmit={(event) => handleSubmit(event)} */}
-          <div className="form-row">
-            <div className="col">
-              <i className="fa fa-user-circle" aria-hidden="true"></i> Name :
-              <input
-                type="text"
-                className="form-control"
-                // onChange={handleInput}
-                placeholder="Name"
-                id="name_id"
-              />
+  changeHandler = (event) => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  submitHandler = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+    axios.post(baseURL,this.state);
+  }
+
+  render() {
+    // const { name, email, dob, age} = this.state
+    return (
+      <div>
+        <div className="boxShadow">
+          <form onSubmit={this.submitHandler}>
+            {/* onSubmit={(event) => handleSubmit(event)} */}
+            <div className="form-row">
+              <div className="col">
+                <i className="fa fa-user-circle" aria-hidden="true"></i> Name :
+                <input
+                  type="text"
+                  className="form-control"
+                  onChange={this.changeHandler}
+                  placeholder="Name"
+                  // value={name}
+                  id="name_id"
+                />
+              </div>
+              <div className="col">
+                <i className="fa fa-envelope" aria-hidden="true"></i> Email :
+                <input
+                  type="email"
+                  onChange={this.changeHandler}
+                  className="form-control"
+                  placeholder="Email"
+                  // value={email}
+                  id="email_id"
+                />
+              </div>
             </div>
-            <div className="col">
-              <i className="fa fa-envelope" aria-hidden="true"></i> Email :
-              <input
-                type="email"
-                // onChange={handleInput}
-                className="form-control"
-                placeholder="Email"
-                id="email_id"
-              />
+            <br />
+            <div className="form-row">
+              <div className="col">
+                <i className="fa fa-birthday-cake" aria-hidden="true"></i> Date
+                of birthday :
+                <input
+                  type="date"
+                  onChange={this.changeHandler}
+                  className="form-control"
+                  // value={dob}
+                  id="dob_id"
+                />
+              </div>
+              <div className="col">
+                <i className="fa fa-user-plus" aria-hidden="true"></i> Age :
+                <input
+                  type="number"
+                  onChange={this.changeHandler}
+                  className="form-control"
+                  placeholder="Age"
+                  // value={age}
+                  id="age_id"
+                />
+              </div>
             </div>
-          </div>
-          <br />
-          <div className="form-row">
-            <div className="col">
-              <i className="fa fa-birthday-cake" aria-hidden="true"></i> Date of
-              birthday :
-              <input
-                type="date"
-                // onChange={handleInput}
-                className="form-control"
-                id="dob_id"
-              />
+            <br />
+            <div className="center">
+              <button
+                // onClick={handleSubmit}
+                type="submit"
+                className="btn btn-primary"
+                // id="submit_id"
+              >
+                Submit
+              </button>
             </div>
-            <div className="col">
-              <i className="fa fa-user-plus" aria-hidden="true"></i> Age :
-              <input
-                type="number"
-                // onChange={handleInput}
-                className="form-control"
-                placeholder="Age"
-                id="age_id"
-              />
-            </div>
-          </div>
-          <br />
-          <div className="center">
-            <button onClick={handleSubmit} type="submit" className="btn btn-primary" id="submit_id">
-              Submit
-            </button>
-          </div>
-          <br />
-        </form>
+            <br />
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
