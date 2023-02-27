5fd6a96fdc80db062647ee4d0c0709358b8920d9
Admin <Badr.Ouaddah.07@gmail.com>
2023-02-27 18:42:42 +0100

    Show and hide another component on Click button in Student app

--- front-end/student-react/src/components/ShowUpdateForm/ShowUpdateForm.jsx
+++ front-end/student-react/src/components/ShowUpdateForm/ShowUpdateForm.jsx
@@ -1,38 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ShowUpdateForm.css";
 
 export default function ShowUpdateForm() {
+  const [isShown, setIsShown] = useState(false);
+
+  const handleClick = (event) => {
+    setIsShown((current) => !current);
+  };
+
   return (
     <div>
-      <div>
-        <div className="card text-left">
-          <div className="card-body colorCard">
-            Name:
-            <input className="form-control" type="text" />
-            <br />
-            Email:
-            <input className="form-control" type="text" />
-            <br />
-            Date of birthday
-            <input className="form-control" type="date" />
-            <br />
-            Age:
-            <input className="form-control" type="text" />
-            <br />
+      <button className="btn btn-warning" onClick={handleClick}>
+        <span className="fa fa-refresh"></span> UPDATE
+      </button>
+      <br />
+      <br />
+      {isShown && (
+        <div>
+          <div>
+            <div className="card text-left">
+              <div className="card-body colorCard">
+                Name:
+                <input className="form-control" type="text" />
+                <br />
+                Email:
+                <input className="form-control" type="text" />
+                <br />
+                Date of birthday
+                <input className="form-control" type="date" />
+                <br />
+                Age:
+                <input className="form-control" type="text" />
+                <br />
+              </div>
+            </div>
+          </div>
+          <br />
+          <div>
+            <input
+              name=""
+              id=""
+              className="btn btn-primary"
+              type="button"
+              value="OK"
+            />
           </div>
         </div>
-      </div>
-      <br />
-      {/* //! button for submit update  */}
-      <div>
-        <input
-          name=""
-          id=""
-          className="btn btn-primary"
-          type="button"
-          value="OK"
-        />
-      </div>
+      )}
     </div>
   );
 }
