c7347dee254f8d142efc6fb4420ecff458b72b27
Admin <Badr.Ouaddah.07@gmail.com>
2023-02-27 17:28:23 +0100

    Add update compenent to edit data student

--- front-end/student-react/src/components/ShowUpdateForm/ShowUpdateForm.jsx
+++ front-end/student-react/src/components/ShowUpdateForm/ShowUpdateForm.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import './ShowUpdateForm.css';
+import React from "react";
+import "./ShowUpdateForm.css";
 
 export default function ShowUpdateForm() {
   return (
     <div>
-        Show update form is ready !
+      <div>
+        <div className="card text-left">
+          <div className="card-body colorCard">
+            Name:
+            <input className="form-control" type="text" />
+            <br />
+            Email:
+            <input className="form-control" type="text" />
+            <br />
+            Date of birthday
+            <input className="form-control" type="date" />
+            <br />
+            Age:
+            <input className="form-control" type="text" />
+            <br />
+          </div>
+        </div>
+      </div>
+      <br />
+      {/* //! button for submit update  */}
+      <div>
+        <input
+          name=""
+          id=""
+          className="btn btn-primary"
+          type="button"
+          value="OK"
+        />
+      </div>
     </div>
-  )
+  );
 }
