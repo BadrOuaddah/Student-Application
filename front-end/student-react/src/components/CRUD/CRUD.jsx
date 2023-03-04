6bc72e843d2cff3a7abe03f10c428cba29c6be00
Admin <Badr.Ouaddah.07@gmail.com>
2023-03-04 19:03:08 +0100

    Add example in CRUD component

--- front-end/student-react/src/components/CRUD/CRUD.jsx
+++ front-end/student-react/src/components/CRUD/CRUD.jsx
@@ -1,16 +1,54 @@
-#!/usr/bin/node
-import React from 'react';
+import React, { useState } from "react";
 import "./CRUD.css";
-import Student from '../Student/Student';
+// import Student from '../Student/Student';
 
 export default function CRUD() {
-    const studentCRUD = Student;
-    console.log(studentCRUD);
-
+  const list = [
+    {
+      id: 1,
+      name: "HP",
+      price: 1400,
+    },
+    {
+      id: 2,
+      name: "DELL",
+      price: 2500,
+    },
+    {
+      id: 3,
+      name: "LENOVO",
+      price: 1200,
+    },
+    {
+      id: 4,
+      name: "ASUS",
+      price: 1700,
+    },
+    {
+      id: 5,
+      name: "ACER",
+      price: 1600,
+    },
+  ];
+  const [lists, setList] = useState(list);
 
   return (
     <div>
-      
+      <table>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>PRICE</th>
+            </tr>
+        {lists.map((current) => (
+            <tr>
+              <td>{current.id}</td>
+              <td>{current.name}</td>
+              <td>{current.price}</td>
+            </tr>
+       
+        ))}
+      </table>
     </div>
-  )
+  );
 }
