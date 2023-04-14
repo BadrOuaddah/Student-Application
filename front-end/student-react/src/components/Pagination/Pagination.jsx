ffc1b62481ce866fc8890e2d3fc75493d20fed84
Admin <Badr.Ouaddah.07@gmail.com>
2023-04-14 16:32:33 +0000

    Update react-paginate package to last version v8.2

--- front-end/student-react/src/components/Pagination/Pagination.jsx
+++ front-end/student-react/src/components/Pagination/Pagination.jsx
@@ -1,23 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Pagination.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { PaginationControl } from "react-bootstrap-pagination-control";
 
 export default function Pagination() {
-  const [page, setPage] = useState(1);
   return (
     <div>
-      <PaginationControl
-        page={page}
-        between={4}
-        total={250}
-        limit={20}
-        changePage={(page) => {
-          setPage(page);
-          console.log(page);
-        }}
-        ellipsis={1}
-      />
+      <h3>Pagination is ready !</h3>
     </div>
   );
 }
