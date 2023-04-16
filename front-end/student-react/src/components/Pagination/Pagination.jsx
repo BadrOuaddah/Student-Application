42baceeb1e9e9fa275c8f45eb024dada5f1bac22
Admin <Badr.Ouaddah.07@gmail.com>
2023-04-16 01:47:33 +0000

    Add pagination V5

--- front-end/student-react/src/components/Pagination/Pagination.jsx
+++ front-end/student-react/src/components/Pagination/Pagination.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ReactPaginate from "react-paginate";
+import ShowUpdateForm from "../ShowUpdateForm/ShowUpdateForm";
+import ShowEditForm from "../ShowEditForm/ShowEditForm";
 
 export default function Pagination({ students }) {
   const [currentItems, setCurrentItems] = useState(null);
@@ -9,35 +11,62 @@ export default function Pagination({ students }) {
 
   useEffect(() => {
     const endOffset = itemOffset + itemsPerPage;
-    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
     setCurrentItems(students.slice(itemOffset, endOffset));
     setPageCount(Math.ceil(students.length / itemsPerPage));
   }, [itemOffset, itemsPerPage, students]);
 
   const handlePageClick = (event) => {
     const newOffset = event.selected * itemsPerPage;
-    console.log(
-      `User requested page number ${event.selected}, which is offset ${newOffset}`
-    );
     setItemOffset(newOffset);
   };
 
   return (
     <>
-      <ul>
-        {currentItems &&
-          currentItems.map((student) => (
-            <li key={student.id}>
-              <ul className="list-group-item list-group-item-action list-group-item-secondary">
-              <li>Id : {student.id}</li>
-              <li>Name : {student.name}</li>
-              <li>Email : {student.email}</li>
-              <li>Date of birthday : {student.dob}</li>
-              <li>Age : {student.age}</li>
-              </ul>
-            </li>
-          ))}
-      </ul>
+      <div className="container">
+        <div className="col card row bg-transparent solid-gray">
+          <div className="card-body">
+            <h4 style={{ textAlign: "center" }} className="card-title">
+              List of students
+            </h4>
+            <br />
+            <div>
+              <ShowEditForm />
+            </div>
+            <hr />
+          </div>
+          <ul className="list-group list-group-flush">
+            {currentItems &&
+              currentItems.map((student) => (
+                <li key={student.id}>
+                  <ul className="list-group-item list-group-item-action list-group-item-secondary">
+                    <li>Id : {student.id}</li>
+                    <li>Name : {student.name}</li>
+                    <li>Email : {student.email}</li>
+                    <li>Date of birthday : {student.dob}</li>
+                    <li>Age : {student.age}</li>
+                    <br />
+                    <div>
+                      <button
+                        onClick={(e) => this.deleteStudent(student.id, e)}
+                        className="btn btn-danger"
+                      >
+                        <span className="fa fa-trash"></span> DELETE
+                      </button>
+                    </div>
+                    <br />
+                    <ShowUpdateForm
+                      {...student}
+                      onUpdate={(updatedStudent, id) =>
+                        this.updateStudent(updatedStudent, id)
+                      }
+                    />
+                    <br />
+                  </ul>
+                </li>
+              ))}
+          </ul>
+        </div>
+      </div>
       <ReactPaginate
         nextLabel="next >"
         onPageChange={handlePageClick}
