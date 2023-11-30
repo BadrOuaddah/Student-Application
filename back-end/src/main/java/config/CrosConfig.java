cd45bb526615cd7bb61468976f979eb3fbfa89ee
Badr Ouaddah <119801735+BadrOuaddah@users.noreply.github.com>
2023-11-30 23:33:20 +0100

    Remove comment in CrosConfig.java

--- back-end/src/main/java/config/CrosConfig.java
+++ back-end/src/main/java/config/CrosConfig.java
@@ -21,21 +21,3 @@ public class CrosConfig {
         };
     }
 }
-
-
-//package in.keepgrowing.springbootangularscaffolding.config;
-//
-//        import org.springframework.context.annotation.Configuration;
-//        import org.springframework.context.annotation.Profile;
-//        import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//        import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//@Profile("development")
-//public class DevCorsConfiguration implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/api/**");
-//    }
-//}
